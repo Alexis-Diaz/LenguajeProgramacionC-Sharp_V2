06bc782215b9906a6d111f6f269b08e848408bca
Alexis Diaz <alex.d4556@gmail.com>
2024-06-04 11:10:12 -0600

    Conversion de datos

---
+++ 08-ConversionesDeDatos/Program.cs
@@ -0,0 +1,45 @@
+﻿//La conversion de datos se presenta cuando queremos
+//pasar un tipo de datos a otro tipo de datos.
+
+//En C# existen dos tipos de conversiones:
+
+//Conversion Implcita
+//Se da cuando se convierte de un tipo de dato pequeño
+//a otro de mayor tamaño
+//char -> int -> long -> float -> double
+int miEntero = 9;
+double miDouble = miEntero; //Automaticamente se convierte
+                            //de int a double
+Console.WriteLine("CONVERSION IMPLICITA (AUTOMATICA)");
+Console.WriteLine(miEntero);
+Console.WriteLine(miDouble);
+
+char miChar = '8';
+int miInt = miChar;
+Console.WriteLine(miChar);
+Console.WriteLine(miInt);
+Console.WriteLine();
+
+
+//Conversion Explicita
+//Se da cuando se convierte de un tipo de datos de mayot
+//tamaño a uno de menor tamaño.
+//double -> float -> long -> int -> char
+double peso = 64.6;
+int peso2 = (int)peso;
+Console.WriteLine("CONVERSION EXPLICITA (MANUAL)");
+Console.WriteLine(peso);
+Console.WriteLine(peso2);
+
+//Las conversiones explicitas tambien se pueden lograr con metodos de conversion.
+//Esto es especualmente util cuando los tipos a convertir son incompatibles.
+
+//string miNumero = "3488";
+//int miNumero2 = (int)miNumero;//Error en tiempo de compilacion
+
+string miNumero = "3488";
+int miNumero2 = Convert.ToInt32(miNumero);//Error en tiempo de compilacion
+Console.WriteLine(miNumero);
+Console.WriteLine(miNumero2);
+
+
---
+++ VariablesIII/Program.cs
@@ -0,0 +1,21 @@
+﻿//CONSTANTES
+//Las constantes son valores inmutables que se conocen en tiempo de compilación y
+//que no cambian durante la vida del programa. Las constantes se declaran con el
+//modificador const.
+//Solo los tipos integrados de C# se pueden declarar como const. Las constantes
+//de tipo de referencia que no sean String solo se pueden inicializar con un
+//valor null. 
+
+//Se debe declarar e inicializar de una vez.
+
+const int MiConstante = 70;
+Console.WriteLine(MiConstante);
+
+const MiClase miClase = null; //El valor asignado solo puede ser null aqui.
+Console.WriteLine(miClase == null ? "null" : miClase.MyProperty);
+
+
+class MiClase
+{
+    public string MyProperty { get; set; }
+}
