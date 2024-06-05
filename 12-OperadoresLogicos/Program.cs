4407f39f4ece23128ed1dab33a3c1776dee6472d
Alexis Diaz <alex.d4556@gmail.com>
2024-06-05 17:11:01 -0600

    Operadores logicos

---
+++ 12-OperadoresLogicos/Program.cs
@@ -0,0 +1,4 @@
+﻿// Operator | Name	        | Description                     	                       |   Example
+// && 	    | Logical and	| Returns True if both statements are true	               |   x < 5  &&  x < 10	
+// || 	    | Logical or	| Returns True if one of the statements is true	           |   x < 5  ||  x < 4	
+// !	    | Logical not	| Reverse the result, returns False if the result is true  | !(x < 5  &&  x < 10)
