66e37644e8050fc91a2f0709985bab4660f361bf
Alexis Diaz <alex.d4556@gmail.com>
2024-06-07 11:39:03 -0600

    Operadores logicos

--- 12-OperadoresLogicos/Program.cs
+++ 12-OperadoresLogicos/Program.cs
@@ -2,3 +2,13 @@
 // && 	    | Logical and	| Returns True if both statements are true	               |   x < 5  &&  x < 10	
 // || 	    | Logical or	| Returns True if one of the statements is true	           |   x < 5  ||  x < 4	
 // !	    | Logical not	| Reverse the result, returns False if the result is true  | !(x < 5  &&  x < 10)
+
+//Ejemplo
+int x = 5;
+int y = 6;
+int z = 4;
+Console.WriteLine(x < y && x < z); //false
+
+Console.WriteLine(x < y || x < z); // true
+
+Console.WriteLine(!(x < y && x < z));// false pero invertido es true
